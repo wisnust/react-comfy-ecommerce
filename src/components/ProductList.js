722ed508719ff62e8e7fbff1d61f3ba6500f9ef1
Wisnu Satrio T <wisnu21cool@gmail.com>
2022-12-26 04:26:43 +0700

    avoid error Products search

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,6 +5,13 @@ import ListView from './ListView'
 
 const ProductList = () => {
   const { filtered_products: products, grid_view } = useFilterContext()
+  if (products.length < 1) {
+    return (
+      <h5 style={{ textTransform: 'none' }}>
+        Sorry, no products matched your search...
+      </h5>
+    )
+  }
   if (grid_view === false) {
     return <ListView products={products} />
   }
