c8675d31df01a1ebede240eb168db6091982ab24
Wisnu Satrio T <wisnu21cool@gmail.com>
2022-12-27 02:11:21 +0700

    Cart: Cart page

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -5,7 +5,30 @@ import { Link } from 'react-router-dom'
 import { CartContent, PageHero } from '../components'
 
 const CartPage = () => {
-  return <h4>cart page</h4>
+  const { cart } = useCartContext()
+  if (cart.length < 1) {
+    return (
+      <main>
+        <PageHero title='Cart' />
+        <Wrapper className='page-100'>
+          <div className='empty'>
+            <h2>Your Cart is Empty</h2>
+            <Link className='btn' to='/products'>
+              Go to Products Page
+            </Link>
+          </div>
+        </Wrapper>
+      </main>
+    )
+  }
+  return (
+    <main>
+      <PageHero title='Cart' />
+      <Wrapper className='page'>
+        <CartContent />
+      </Wrapper>
+    </main>
+  )
 }
 
 const Wrapper = styled.main`
