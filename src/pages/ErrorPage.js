832c78cfbd486f45ccb726c1e7e17f91c41a7ba7
Wisnu Satrio T <wisnu21cool@gmail.com>
2022-12-27 04:31:19 +0700

    Error Page: add back button

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -7,6 +7,9 @@ const ErrorPage = () => {
       <section>
         <h1>404</h1>
         <p>Page not found</p>
+        <Link to='/' className='btn'>
+          Go back Home
+        </Link>
       </section>
     </Wrapper>
   )
